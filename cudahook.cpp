e0ffc92046c0641ad2fce1d6e92ab7cc6e270439
Nathan <nathan.chong@gmail.com>
2013-08-08 00:05:18 +0100

    Make sure static variables are initialised before use.
    http://www.parashift.com/c++-faq-lite/static-init-order-on-first-use.html

--- cudahook.cpp
+++ cudahook.cpp
@@ -7,18 +7,27 @@
 #include <cuda.h>
 #include <vector_types.h>
 
-static struct {
+typedef struct {
   dim3 gridDim;
   dim3 blockDim;
   int counter;
   std::list<void *> args;
-} kernelInfo = { 0, 0, 0 };
-std::map<const char *, char *> kernels;
+} kernelInfo_t;
+
+kernelInfo_t &kernelInfo() {
+  static kernelInfo_t _kernelInfo;
+  return _kernelInfo;
+}
+
+std::map<const char *, char *> &kernels() {
+  static std::map<const char*, char*> _kernels;
+  return _kernels;
+}
 
 void print_kernel_invocation(const char *entry) {
-  dim3 gridDim = kernelInfo.gridDim;
-  dim3 blockDim = kernelInfo.blockDim;
-  printf("SENTINEL %s ", kernels[entry]);
+  dim3 gridDim = kernelInfo().gridDim;
+  dim3 blockDim = kernelInfo().blockDim;
+  printf("SENTINEL %s ", kernels()[entry]);
   if (gridDim.y == 1 && gridDim.z == 1) {
     printf("--gridDim=%d ", gridDim.x);
   } else if (gridDim.z == 1) {
@@ -33,11 +42,11 @@ void print_kernel_invocation(const char *entry) {
   } else {
     printf("--blockDim=[%d,%d,%d] ", blockDim.x, blockDim.y, blockDim.z);
   }
-  for (std::list<void *>::iterator it = kernelInfo.args.begin(),
-       end = kernelInfo.args.end();
+  for (std::list<void *>::iterator it = kernelInfo().args.begin(),
+       end = kernelInfo().args.end();
        it != end;
        ++it) {
-    unsigned i = std::distance(kernelInfo.args.begin(), it);
+    unsigned i = std::distance(kernelInfo().args.begin(), it);
     printf("%d:%d ", i, *(static_cast<int *>(*it)));
   }
   printf("\n");
@@ -52,10 +61,10 @@ cudaError_t cudaConfigureCall(
   dim3 blockDim,
   size_t sharedMem=0,
   cudaStream_t stream=0) {
-  assert(kernelInfo.counter == 0 && "Multiple cudaConfigureCalls before cudaLaunch?");
-  kernelInfo.gridDim = gridDim;
-  kernelInfo.blockDim = blockDim;
-  kernelInfo.counter++;
+  assert(kernelInfo().counter == 0 && "Multiple cudaConfigureCalls before cudaLaunch?");
+  kernelInfo().gridDim = gridDim;
+  kernelInfo().blockDim = blockDim;
+  kernelInfo().counter++;
   if (realCudaConfigureCall == NULL)
     realCudaConfigureCall = (cudaConfigureCall_t)dlsym(RTLD_NEXT,"cudaConfigureCall");
   assert(realCudaConfigureCall != NULL && "cudaConfigureCall is null");
@@ -67,9 +76,9 @@ static cudaLaunch_t realCudaLaunch = NULL;
 
 extern "C"
 cudaError_t cudaLaunch(const char *entry) {
-  assert(kernelInfo.counter == 1 && "Multiple cudaConfigureCalls before cudaLaunch?");
+  assert(kernelInfo().counter == 1 && "Multiple cudaConfigureCalls before cudaLaunch?");
   print_kernel_invocation(entry);
-  kernelInfo.counter--; kernelInfo.args.clear();
+  kernelInfo().counter--; kernelInfo().args.clear();
   if (realCudaLaunch == NULL) {
     realCudaLaunch = (cudaLaunch_t)dlsym(RTLD_NEXT,"cudaLaunch");
   }
@@ -86,7 +95,7 @@ extern "C"
 void __cudaRegisterFunction(void **fatCubinHandle, const char *hostFun, char *deviceFun,
                             const char *deviceName, int thread_limit, uint3 *tid,
                             uint3 *bid, dim3 *bDim, dim3 *gDim, int *wSize) {
-  kernels[hostFun] = deviceFun;
+  kernels()[hostFun] = deviceFun;
   if (realCudaRegisterFunction == NULL) {
     realCudaRegisterFunction = (cudaRegisterFunction_t)dlsym(RTLD_NEXT,"__cudaRegisterFunction");
   }
@@ -101,7 +110,7 @@ static cudaSetupArgument_t realCudaSetupArgument = NULL;
 
 extern "C"
 cudaError_t cudaSetupArgument(const void *arg, size_t size, size_t offset) {
-  kernelInfo.args.push_back(const_cast<void *>(arg));
+  kernelInfo().args.push_back(const_cast<void *>(arg));
   if (realCudaSetupArgument == NULL) {
     realCudaSetupArgument = (cudaSetupArgument_t)dlsym(RTLD_NEXT,"cudaSetupArgument");
   }
